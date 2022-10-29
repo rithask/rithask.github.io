67a4872abc688aa293bdcfe7ac04b56bfbffa1d1
Rithas K <rithask@icloud.com>
2022-10-29 10:02:28 +0530

    Code rewrite

--- script.js
+++ script.js
@@ -1,44 +1,8 @@
 $(document).ready(function() {
-	$("#adviceBtn").click(function() {
-		$.ajax({
-			url: 'https://api.adviceslip.com/advice',
-			type: 'GET',
-			dataType: 'json',
-			success: function(response) {
-				$("#adviceBox").html(response.slip.advice);
-			}
-		});
-	});
-
-	$("#dropdownMenu a").on('click', function(e) {
-		e.preventDefault();
-		var tag = $(this).text();
-		$("#dropdownMenuButton").html(tag);
-		
-		$.ajax({
-			url: 'https://api.quotable.io/random?tags=' + tag.toLowerCase(),
-			type: 'GET',
-			dataType: 'json',
-			success: function(response) {
-				quote = response.content;
-				author = response.author;
-				$("#quoteBox").html(quote);
-				$("#quoteAuthor").html('-' + author);
-			}
-		})
-	})
-
-	$("#meow").on('click', function() {
-		$.ajax({
-			url: 'https://cataas.com/cat?json=true',
-			dataType: 'json',
-			type: 'GET',
-			success: function(response) {
-				cat = response.url;
-				catUrl = ('https://cataas.com' + cat);
-				console.log(catUrl);
-				$("#catImg").attr('src', catUrl);
-			}
-		})
-	})
+    // Toggle dark mode based on time of the day
+    var hour = new Date().getHours();
+    const darkMode = hour >= 18 || hour <= 6 ? true : false;
+    if (darkMode === true) {
+        $('html').addClass('dark-mode');
+    }
 });
